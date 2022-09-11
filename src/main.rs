149c784698523779a99170885c4a1440407e1882
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-09-11 23:25:50 +0900

    stringの基礎

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use string::string_fn;
+
 mod array_module;
+mod string;
 mod tuple_module;
 mod vector_module;
 
@@ -6,8 +9,9 @@ mod calc_module;
 
 fn main() {
     // 絶対パスで指定
-    crate::tuple_module::tuple_fn();
+    // crate::tuple_module::tuple_fn();
     // 相対パスで指定（self::は省略できる）
-    array_module::array_fn();
-    vector_module::vector_fn();
+    // array_module::array_fn();
+    // vector_module::vector_fn();
+    string_fn();
 }
