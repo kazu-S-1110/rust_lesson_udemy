098b9ea495d0514756224390eba8c6345bd28cee
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-10-08 22:40:17 +0900

    matchをtupleで

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -9,3 +9,16 @@ pub fn fizzbuzz(num: i32) {
         println!("{}", num)
     }
 }
+
+pub fn fizzbuzz_v2(num: i32) {
+    let r = 1..=num;
+
+    for x in r {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", x),
+        }
+    }
+}
