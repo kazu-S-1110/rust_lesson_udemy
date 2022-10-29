9b4623cdb86772fe65746aa5a13257b1a03fd3ca
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-10-29 23:13:07 +0900

    binaryの扱いについて学習

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-use string::string_fn;
-mod array_module;
-mod basic_struct_sec;
-mod calc_module;
-mod enum_sec;
-mod fizzbuzz;
-mod function;
-mod if_section;
-mod loop_section;
-mod match_section;
-mod optional_sec;
-mod ref_section;
-mod smart_pointer_section;
-mod string;
-mod sum;
-mod tuple_module;
-mod vector_module;
-use rand::Rng;
-
 fn main() {
-    // 絶対パスで指定
-    // crate::tuple_module::tuple_fn();
-    // 相対パスで指定（self::は省略できる）
-    // array_module::array_fn();
-    // vector_module::vector_fn();
-    // string_fn();
-    // if_section::if_section()
-    // loop_section::loop_section()
-    // smart_pointer_section::smart_pointer_section();
-    // basic_struct_sec::basic_struct_sec()
-    // enum_sec::enum_sec()
-    // optional_sec::optional_sec()
-    let random_num = rand::thread_rng().gen_range(1..101);
-    println!("{}", random_num);
+    println!("This is main")
 }
