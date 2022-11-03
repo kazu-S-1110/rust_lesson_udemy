7666fc7d101ab9fa98721ce187e9d96f5408ec47
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-11-03 23:54:16 +0900

    traitを定義して実装まで

--- src/lib.rs
+++ src/lib.rs
@@ -23,4 +23,39 @@ pub mod sample_trait {
         fn calc_perimeter(&self) -> f64;
         fn do_something();
     }
+
+    pub struct Rectangle {
+        pub width: f64,
+        pub height: f64,
+    }
+
+    impl Shape for Rectangle {
+        fn calc_area(&self) -> f64 {
+            self.height * self.width
+        }
+
+        fn calc_perimeter(&self) -> f64 {
+            self.height * 2.0 + self.width * 2.0
+        }
+
+        fn do_something() {
+            println!("This is Rectangle func")
+        }
+    }
+
+    pub struct Circle {
+        pub raidus: f64,
+    }
+
+    impl Shape for Circle {
+        fn calc_area(&self) -> f64 {
+            self.raidus * self.raidus * std::f64::consts::PI
+        }
+        fn calc_perimeter(&self) -> f64 {
+            self.raidus * 2.0 * std::f64::consts::PI
+        }
+        fn do_something() {
+            println!("This is Circle func")
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use rust_lesson_udemy::sample_trait::{Circle, Rectangle, Shape};
+
 fn main() {
-    println!("This is main");
-    rust_lesson_udemy::say_hello()
+    let rect = Rectangle {
+        height: 4.0,
+        width: 2.0,
+    };
+
+    let circle = Circle { raidus: 5.0 };
+
+    println!("rect area is {}", rect.calc_area());
+    println!("circle area is {}", circle.calc_area());
 }
