dfef9cabd21c7190435b05d945cb6502f1754c9a
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-11-04 12:37:42 +0900

    SMCC

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,3 @@
-//! ライブラリクレートのドキュメント（このコメントはライブラリ全体にかかるコメント
-
-/// これはクレートにかかるコメント
-pub fn say_hello() {
-    println!("Hello Rust!")
-    // binは相互に干渉はできないがlibは全てに干渉が可能
-}
-
-/// **say_good_by**関数のドキュメント
-/// ### Usage
-/// ```
-/// fn main(){
-///     rust_lesson_udemy::say_good_bye()
-/// }
-/// ````
-pub fn say_good_bye() {
-    println!("Good bye")
-}
-
 pub mod sample_trait {
     pub trait Shape {
         fn calc_area(&self) -> f64;
